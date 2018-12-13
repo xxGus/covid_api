5fd09d4a13bd7111c2a2a06d4cb00954d2956a1d
Victor <victorpassis@gmail.com>
2018-12-13 13:50:53 -0200

    first commit

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 // Setup server port
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
 
 
 // Send message for default URL
