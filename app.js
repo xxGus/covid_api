5d2133a838edfd2e7203b8b128c91a7555b2a0c5
Victor <victorpassis@gmail.com>
2018-12-14 12:09:19 -0200

    Adjusted

--- app.js
+++ app.js
@@ -19,6 +19,25 @@ app.use(bodyParser.json());
 //var port = process.env.PORT || 8080;
 var port = process.env.PORT || 3000;
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://list-repositories.herokuapp.com');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 
 // Send message for default URL
 app.get('/', (req, res) => res.send('Hello World with Express'));
